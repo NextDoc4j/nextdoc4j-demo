d8b518170deae66e1e29736a1510d6ff58ebe23d
吴泽威 <958142070@qq.com>
2025-11-24 19:51:49 +0800

    feat(app): 添加根路径接口返回项目信息
    - 新增 RestController 标注于主应用类以支持 HTTP 接口
    - 添加 ProjectProperties 依赖注入以获取配置属性
    - 实现根路径 GET 请求处理方法，返回项目配置信息的标准响应对象
    - 使用 @Hidden 注解隐藏该接口在 Swagger 文档中显示

--- src/main/java/top/nextdoc4j/demo/Nextdoc4jDemoApplication.java
+++ src/main/java/top/nextdoc4j/demo/Nextdoc4jDemoApplication.java
@@ -1,10 +1,27 @@
 package top.nextdoc4j.demo;
 
+import io.swagger.v3.oas.annotations.Hidden;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import top.nextdoc4j.demo.configuration.properties.ProjectProperties;
+import top.nextdoc4j.demo.model.base.R;
 
+@RequiredArgsConstructor
 @SpringBootApplication
+@RestController
 public class Nextdoc4jDemoApplication {
+
+    private final ProjectProperties projectProperties;
+
+
+    @Hidden
+    @GetMapping("/")
+    public R index() {
+        return R.ok(projectProperties);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Nextdoc4jDemoApplication.class, args);
